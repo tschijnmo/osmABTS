4f7702481e3fec9b5b86bf83847ecf1d30443f4a
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2014-12-08 15:51:56 -0600

    Rewrite the network formation module
    The network formation module has been completely rewritten. Now the
    junction recognition is no longer based on the ``highway`` tags, but
    rather use sophisticated code to remove nodes with only two neighbours.

--- osmABTS/network.py
+++ osmABTS/network.py
@@ -38,15 +38,6 @@ import matplotlib.pyplot as plt
 # -----------------------------------------------
 #
 
-# if the ``highway`` key contains the follow value for a node in raw OSM, then
-# it is considered a node in the network.
-_NODES_TAGS = [
-    'traffic_signals',
-    'crossing',
-    'turning_circle',
-    'motorway_junction',
-    ]
-
 # The speed to travel on each kind of highways
 # In miles per hour
 _HIGHWAY_SPEEDS = {
@@ -54,10 +45,10 @@ _HIGHWAY_SPEEDS = {
     'primary': 40.0,
     'primary_link': 40.0,
     'secondary': 35.0,
+    'secondary_link': 35.0,
     'tertiary': 30.0,
-    'footway': 35.0,
-    'service': 35.0,
     'motorway': 70.0,
+    'motorway_link': 70.0,
 }
 
 
@@ -65,14 +56,6 @@ _HIGHWAY_SPEEDS = {
 # Utility functions
 # -----------------
 #
-
-def _test_if_node(node):
-
-    """Tests if a node in the raw OSM data a node in the network"""
-
-    tags = node.tags
-    return 'highway' in tags and tags['highway'] in _NODES_TAGS
-
 
 def _calc_distance(coord1, coord2):
 
@@ -86,17 +69,30 @@ def _calc_distance(coord1, coord2):
     return vincenty(coord1, coord2).miles
 
 
+def _test_if_road(way):
+
+    """Tests if a raw way is a road to consider"""
+
+    tags = way.tags
+    return 'highway' in tags and tags['highway'] not in [
+        'footway',
+        'service',
+        ]
+
+
 #
 # The driver function
 # -------------------
 #
 
-def form_network_from_osm(raw_osm):
+def form_network_from_osm(raw_osm, trim=True):
 
     """Forms a road network from the raw OSM data
 
     :param raw_osm: A :py:class:`osmABTS.readosm.RawOSM` instance for the raw
         data
+    :param trim: Boolean value indicating if pure connection nodes are going to
+        be trimmed out.
     :returns: A networkX graph for the road connectivity
 
     """
@@ -106,20 +102,18 @@ def form_network_from_osm(raw_osm):
     # nodes formation
     nodes = raw_osm.nodes
     for node_id, node in nodes.iteritems():
-        if _test_if_node(node):
-            net.add_node(node_id)
-            net.node[node_id]['coord'] = node.coord
-        continue
+        net.add_node(node_id)
+        net.node[node_id]['coord'] = node.coord
 
     # edge formation
     for way in raw_osm.ways.itervalues():
 
         # test if it is actually a road
-        tags = way.tags
-        if 'highway' in tags:
-            highway = tags['highway']
+        if not _test_if_road(way):
+            continue
         else:
-            continue  # building or something like that
+            tags = way.tags
+            highway = tags['highway']
 
         # connect the nodes in the network
 
@@ -133,27 +127,61 @@ def form_network_from_osm(raw_osm):
 
             # Update the distance
             curr_coord = node.coord
-            distance += _calc_distance(curr_coord, prev_coord)
+            distance = _calc_distance(curr_coord, prev_coord)
             prev_coord = curr_coord
 
-            if _test_if_node(node):
-                # add edge if there is a previous node
-                if prev_node_id is not None:
-                    # Add the new edge
-                    try:
-                        travel_time = distance / _HIGHWAY_SPEEDS[highway]
-                    except IndexError:
-                        raise IndexError(
-                            'Unknown highway type %s' % highway
-                            )
-                    net.add_edge(
-                        node_id, prev_node_id,
-                        travel_time=travel_time, length=distance,
-                        highway=highway, name=tags.get('name', '')
+            # add edge if there is a previous node
+            if prev_node_id is not None:
+                # Add the new edge
+                try:
+                    travel_time = distance / _HIGHWAY_SPEEDS[highway]
+                except IndexError:
+                    raise IndexError(
+                        'Unknown highway type %s' % highway
                         )
-                # Update previous node no matter there is a previous one
-                prev_node_id = node_id
-                distance = 0.0
+                net.add_edge(
+                    node_id, prev_node_id,
+                    travel_time=travel_time, length=distance,
+                    highway=highway, name=tags.get('name', '')
+                    )
+
+            prev_node_id = node_id
+
+    # Remove unconnected nodes
+    # They are generally utility nodes for purposes other than defining roads
+    net.remove_nodes_from(
+        [i for i in net.nodes_iter() if len(net[i]) == 0]
+        )
+
+    # Iterate through the ways once again to remove nodes that serve only to
+    # connect two points.
+    for way in raw_osm.ways.itervalues():
+
+        if not _test_if_road(way):
+            continue
+
+        for node_id in way.nodes:
+            try:
+                neighb = net[node_id]
+            except KeyError:
+                continue  # skip nodes already deleted
+
+            if len(neighb) != 2:
+                continue
+            else:
+                n1, n2 = neighb.keys()
+                if n2 not in net[n1]:
+                    net.add_edge(
+                        n1, n2,
+                        length=neighb[n1]['length'] + neighb[n2]['length'],
+                        travel_time=(
+                            neighb[n1]['travel_time'] +
+                            neighb[n2]['travel_time']
+                            ),
+                        highway=neighb[n1]['highway'],
+                        name=neighb[n1]['name']
+                        )
+                net.remove_node(node_id)
 
     return net
 
@@ -179,7 +207,12 @@ def node2str(net, node):
         attrs['name'] for _, attrs in net[node].iteritems()
         )
 
-    return 'junction of ' + ', '.join(edge_names)
+    if len(edge_names) == 1:
+        prefix = 'point on '
+    else:
+        prefix = 'junction of '
+
+    return prefix + ', '.join(edge_names)
 
 
 def edge2str(net, edge):
@@ -216,7 +249,7 @@ def draw_network(net, out_name):
         for key, data in net.nodes_iter(data=True)
         }
 
-    nx.draw_networkx(net, pos=pos, with_labels=False, node_size=32)
+    nx.draw_networkx(net, pos=pos, with_labels=False, node_size=2)
     plt.axis('off')
     plt.savefig(out_name)
 
