f2d6498b845282292e691bf5317eacb753b003b4
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2014-12-16 03:29:29 -0600

    Conage the attribution in the documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'osmABTS'
-copyright = u'2014, Tschijnmo and Schuw TSCHAU'
+copyright = u'2014, Shuo ZHAO'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -203,7 +203,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'osmABTS.tex', u'osmABTS Documentation',
-   u'Tschijnmo and Schuw TSCHAU', 'manual'),
+   u'Shuo ZHAO', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -233,7 +233,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'osmabts', u'osmABTS Documentation',
-     [u'Tschijnmo and Schuw TSCHAU'], 1)
+     [u'Shuo ZHAO'], 1)
 ]
 
 # If true, show URL addresses after external links.
